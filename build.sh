14a721fa14ed68ff4f9364fcf6879964e074631f
Balogun Sodiq <sodiqb86@gmail.com>
2023-06-18 13:33:16 +0100

    ready

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -o errexit  # exit on error
 
+python -m pip install -r requirements.txt
 
-/usr/bin/pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
