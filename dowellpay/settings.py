9968819880a165f7a9d29922dedf1b8fd8f32c98
Balogun Sodiq <sodiqb86@gmail.com>
2023-06-18 15:22:09 +0100

    ready

--- dowellpay/settings.py
+++ dowellpay/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'dowellpay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+# }
 
 
 # Password validation
