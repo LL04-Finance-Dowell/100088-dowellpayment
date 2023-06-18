bbcfdcdd1576808281aab0b2c4f53886a5c6ebed
Balogun Sodiq <sodiqb86@gmail.com>
2023-06-18 12:00:11 +0100

    ready

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -o errexit  # exit on error
 
-python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools
-python -m pip install -r requirements.txt
+python -m pip3 install --upgrade pip
+python -m pip3 install --upgrade setuptools
+python -m pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
