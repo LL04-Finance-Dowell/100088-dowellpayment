439b0ae5188c974685d0d0af9041cdc188e7e3c0
Balogun Sodiq <sodiqb86@gmail.com>
2023-06-18 12:19:08 +0100

    ready

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -o errexit  # exit on error
 pip3 install --upgrade pip
 pip3 install --upgrade setuptools
 
-/usr/bin/python3 -m pip install -r requirements.txt
 export PATH="/opt/render/.local/bin:$PATH"
+/usr/bin/python3 -m pip install -r requirements.txt
+
 
 
 
