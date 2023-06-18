d7cc8c8facf2a0784df7a127fd4cb60af823a78e
Balogun Sodiq <sodiqb86@gmail.com>
2023-06-18 12:17:46 +0100

    ready

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ pip3 install --upgrade pip
 pip3 install --upgrade setuptools
 
 /usr/bin/python3 -m pip install -r requirements.txt
+export PATH="/opt/render/.local/bin:$PATH"
+
 
 
 python manage.py collectstatic --no-input
