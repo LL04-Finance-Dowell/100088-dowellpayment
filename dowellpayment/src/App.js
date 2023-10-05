19c3c844c205baa658b8662a8322ab13c294596b
Ololade Otemade <adeniyiotemade@gmail.com>
2023-10-05 11:28:50 +0100

    Left column complete

--- dowellpayment/src/App.js
+++ dowellpayment/src/App.js
@@ -1,8 +1,56 @@
 
 function App() {
   return (
-    <div>
-      <h1>Dowell Payment</h1>
+    <div className="container">
+      {/* left col */}
+      <div className="left-col">
+        <div>
+          <img src='/dowell-logo.svg' alt='Company logo' className="logo" />
+          <h1 className="title">DoWell World Price Indicator</h1>
+          <p className='desc'>Purchase Price Parity Calculator</p>
+        </div>
+
+        <form className="form">
+          <div className="form-group">
+            <label>Base Currency</label>
+            <select>
+
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Base Price</label>
+            <select>
+              
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Base Country</label>
+            <select>
+              
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Target Country</label>
+            <select>
+              
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Target Currency</label>
+            <select>
+              
+            </select>
+          </div>
+          <button>
+            Calculate
+          </button>
+        </form>
+      </div>
+
+      {/* right col */}
+      <div className="right-col">
+        <h1>Dowell Payment</h1>
+      </div>
     </div>
   );
 }
