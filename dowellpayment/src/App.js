639133ba80d83ac4e7f4dc9e21288dd92fa82760
Ololade Otemade <adeniyiotemade@gmail.com>
2023-10-05 13:19:15 +0100

    Desktop view

--- dowellpayment/src/App.js
+++ dowellpayment/src/App.js
@@ -14,7 +14,8 @@ function App() {
           <div className="form-group">
             <label>Base Currency</label>
             <select>
-
+              <option>Test</option>
+              <option>Test2</option>
             </select>
           </div>
           <div className="form-group">
@@ -49,7 +50,29 @@ function App() {
 
       {/* right col */}
       <div className="right-col">
-        <h1>Dowell Payment</h1>
+        <img src='/illustration.svg' alt='Company logo' className="logo" />
+        <div className="faq">
+          <div className="section">
+            <p className="question">How can we help?</p>
+            <p className="answer">
+              DoWell World Price indicator is calculating based on Purchasing Power Parity calculation. Use any currency in any country as base then calculate for any country and any currency as target
+            </p>
+          </div>
+
+          <div className="section">
+            <p className="question">How it works?</p>
+            <p className="answer">
+            Scenario- if I am buying a product for 100 USD in Germany, How much I have to pay for the same product in USA in British Pound?
+            </p>
+          </div>
+
+          <div className="section">
+            <p className="question">Powered By</p>
+            <p className="answer">
+            Dowell Living lab.....
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
