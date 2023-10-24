6c832dc1c94e81c1f1b3f76cca02c539d04ffddf
Balogun Sodiq <sodiqb86@gmail.com>
2023-10-24 08:01:08 +0100

    working on ppp

--- purchasing_power_parity/urls.py
+++ purchasing_power_parity/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import (
-    GetCurrencyNameAndCountryName,
     GetPurchasingPowerParity,
     GetPublicPurchasingPowerParity,
     SendResponseToClient
@@ -8,7 +7,6 @@ from .views import (
 
 
 urlpatterns = [
-    path("country-currency", GetCurrencyNameAndCountryName.as_view()),
     path("ppp", GetPurchasingPowerParity.as_view()),
     path("ppp/client-mail", SendResponseToClient.as_view()),
     path("ppp/public/<str:api_key>", GetPublicPurchasingPowerParity.as_view()),
--- purchasing_power_parity/views.py
+++ purchasing_power_parity/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PPPSerializer
-from .helper import get_all_currency_and_country, get_all_currency_name, get_ppp_data
+from .helper import  get_all_currency_name, get_ppp_data
 import requests
 import re
 
@@ -17,22 +17,6 @@ def processApikey(api_key):
 
 # FOR DOWELL INTERNAL TEAM
 
-
-class GetCurrencyNameAndCountryName(APIView):
-    def get(self, request):
-        try:
-            #  call the function to get all the currency name
-            res = get_all_currency_and_country()
-            return res
-        except Exception as e:
-            return Response(
-                {
-                    "success": False,
-                    "message": "something went wrong",
-                    "error": f"{e}",
-                },
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
 
 
 class GetPurchasingPowerParity(APIView):
@@ -98,72 +82,150 @@ class GetPurchasingPowerParity(APIView):
             )
 
 class SendResponseToClient(APIView):
-    def post(self,request):
+    def post(self, request):
         try:
             data = request.data
             pattern1 = r"base_price_in_\w+"
             pattern2 = r"calculated_price_in_\w+"
-            
-            base_price_in_base_country =""
+
+            base_price_in_base_country = ""
             calculated_price_in_target_country = ""
             for key, value in data.items():
                 if re.match(pattern1, key):
                     base_price_in_base_country = value
-                    
+
             for key, value in data.items():
                 if re.match(pattern2, key):
                     calculated_price_in_target_country = value
-                    
-                    
-            print("base_price_in_base_country",base_price_in_base_country)
-            print("calculated_price_in_target_country",calculated_price_in_target_country)
-            
-            
+
+            print("base_price_in_base_country", base_price_in_base_country)
+            print(
+                "calculated_price_in_target_country", calculated_price_in_target_country
+            )
+
             email = data["email"]
             base_currency = data["base_currency"]
             target_currency = data["target_currency"]
             exchange_rate = data["exchange_rate"]
-            # base_price_in_base_country = data["base_price_in_base_country"] 
+            # base_price_in_base_country = data["base_price_in_base_country"]
             # calculated_price_in_target_country = data["calculated_price_in_target_country"]
             price_in_base_country = data["price_in_base_country"]
             base_country = data["base_country"]
             target_country = data["target_country"]
             target_price = data["target_price"]
             calculated_price_base_on_ppp = data["calculated_price_base_on_ppp"]
-            
+
             url = "https://100085.pythonanywhere.com/api/email/"
 
             EMAIL_FROM_WEBSITE = """
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>Samanta Content Evaluator</title>
-                        </head>
-                        <body>
-                            <div style="font-family: Helvetica,Arial,sans-serif;min-width:100px;overflow:auto;line-height:2">
-                                <div style="margin:50px auto;width:70%;padding:20px 0">
-                                <div style="border-bottom:1px solid #eee">
-                                    <a href="#" style="font-size:1.2em;color: #00466a;text-decoration:none;font-weight:600">Dowell UX Living Lab</a>
-                                </div>
-                                <p style="font-size:1.1em">Base Country : {base_country}</p>
-                                <p style="font-size:1.1em">Target Country : {target_country}</p>
-                                <p style="font-size:1.1em">Price In Base Country : {price_in_base_country}</p>
-                                <p style="font-size:1.1em">Base Price In {base_country} : {base_price_in_base_country},</p>
-                                <p style="font-size:1.1em">Calculated Price In {target_country} : {calculated_price_in_target_country}</p>
-                                <p style="font-size:1.1em">1 {base_currency} = {exchange_rate} {target_currency}</p>
-                                <p style="font-size:1.1em">Target Price : {target_price}</p>
-                                <p style="font-size:1.1em">Calculated Price Based On PPP : {calculated_price_base_on_ppp}</p>
-                                </div>
-                            </div>
-                        </body>
-                        </html>
-                      """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>DoWell World Price Indicator</title>
+            </head>
+            <body
+                style="
+                font-family: Arial, sans-serif;
+                background-color: #ffffff;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                justify-content: center;
+                "
+            >
+                <div style="width: 100%; background-color: #ffffff;">
+                <header
+                    style="
+                    color: #fff;
+                    display: flex;
+                    text-align: center;
+                    justify-content: center;
+                    padding: 5px;
+                    "
+                >
+                    <img
+                    src="https://dowellfileuploader.uxlivinglab.online/hr/logo-2-min-min.png"
+                    height="80px"
+                    width="140px"
+                    style="display: block; margin: 0 auto;"
+                    />
+                </header>
+                <article style="margin-top: 20px; text-align: center;">
+                    <h2>DoWell World Price Indicator</h2>
+                </article>
+
+                <main style="padding: 20px;">
+                    <section style="margin: 20px;">
+                    <p>Hi {email},</p>
+                    <p>Result from DoWell World Price Indicator :</p>
+                    <p style="text-align: center;">
+                        <span style="font-weight: bold; font-size: 1.2rem;"
+                        >Base Price in {base_country} : {base_price_in_base_country}</span
+                        >
+                    </p>
+                    <p style="text-align: center;">
+                        <span style="font-weight: bold; font-size: 1.2rem;"
+                        >Calculated Price in {target_country} : {calculated_price_in_target_country}</span
+                        >
+                    </p>
+                    <p style="font-weight: bold;">Detailed information :</p>
+                    <ul>
+                        <li>Base Currency : {base_currency}</li>
+                        <li>Base Country : {base_country}</li>
+                        <li>Target Country : {target_country}</li>
+                        <li>Target Price : {target_price}</li>
+                        <li>Price In Base Country : {price_in_base_country}</li>
+                        <li>Base Price In {base_country} : {base_price_in_base_country}</li>
+                        <li>Calculated Price In {target_country} : {calculated_price_in_target_country}</li>
+                        <li>Calculated Price Based On PPP : {calculated_price_base_on_ppp}</li>
+                        <li>Exchange rate between {base_country} and {target_country} : {exchange_rate} </li>
+                    </ul>
+                    <div style="margin: 20px;">
+                        <p>
+                        Thank You for using DoWell World Price Indicator, for more queries
+                        contact
+                        <a href="mailto:dowell@dowellresearch.uk"
+                            >dowell@dowellresearch.uk</a
+                        >
+                        </p>
+                        <p>DoWell UX Living Lab Team</p>
+                    </div>
+                    </section>
+                </main>
+
+                <footer
+                    style="
+                    background-color: #005733;
+                    color: #fff;
+                    text-align: center;
+                    padding: 10px;
+                    "
+                >
+                    <a
+                    href="https://www.uxlivinglab.org/"
+                    style="
+                        text-align: center;
+                        color: white;
+                        margin-bottom: 20px;
+                        padding-bottom: 10px;
+                    "
+                    >DoWell UX Living Lab</a
+                    >
+                    <p style="margin-top: 10px; font-size: 13px;">
+                    &copy; 2023-All rights reserved.
+                    </p>
+                </footer>
+                </div>
+            </body>
+            </html>
+            """
+
             email_content = EMAIL_FROM_WEBSITE.format(
-                base_currency = base_currency,
-                target_currency = target_currency,
+                email=email,
+                base_currency=base_currency,
+                target_currency=target_currency,
                 exchange_rate=exchange_rate,
                 base_country=base_country,
                 base_price_in_base_country=base_price_in_base_country,
@@ -183,12 +245,9 @@ class SendResponseToClient(APIView):
             response = requests.post(url, json=payload)
             print(response.text)
             return Response(
-        {
-            "success": True,
-            "message": "Mail sent successfully"
-        },
-        status=status.HTTP_200_OK,
-    )
+                {"success": True, "message": "Mail sent successfully"},
+                status=status.HTTP_200_OK,
+            )
         except Exception as e:
             return Response(
                 {
